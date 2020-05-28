f6d28bedd0ce5ade0b2b52a2adae9fe36e04e54b
Jules Kulcsar <kulcsar_jules@yahoo.com>
2020-05-28 20:23:17 +0200

    added login route and controllers, pass hash and email and pass validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 //@desc register user
-//@route GET /api/v1/auth/register
+//@route POST /api/v1/auth/register
 //@access public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body
@@ -19,7 +19,43 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     })
 
+    //create token
+    const token = user.getSignedJwtToken();
+
     res.status(200).json({
-        success: true
+        success: true,
+        token: token
+    })
+})
+
+//@desc login user
+//@route POST /api/v1/auth/register
+//@access public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    //validate email and pass
+    if (!email || !password) {
+        return next(new ErrorResponse('please provide an email and password', 400))
+    }
+
+    //check for user
+    const user = await User.findOne({ email: email }).select('+password')
+    if (!user) {
+        return next(new ErrorResponse('please provide a valid email and password', 401))
+    }
+
+    //check if password macthes
+    const isMatch = await user.matchPassword(password)
+    if (!isMatch) {
+        return next(new ErrorResponse('please provide a valid email and password', 401))
+    }
+
+    //create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token: token
     })
 })
