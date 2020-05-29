1503f88d062ad793c23bd66c4e9a503d6883fdb1
Jules Kulcsar <kulcsar_jules@yahoo.com>
2020-05-29 17:29:22 +0200

    added admin user crud functionality routes and controllers

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ connectDB();
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
 const auth = require('./routes/auth')
+const users = require('./routes/users')
 
 const app = express()
 
@@ -46,6 +47,7 @@ app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use('/api/v1/bootcamps/:bootcampId/courses', courses)
 app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
 
 //has to be after mount routes
 app.use(errorHandler)
